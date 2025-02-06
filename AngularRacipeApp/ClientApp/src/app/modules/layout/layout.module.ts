a504d9a9d496271aa5dc62a414d8477eb8cceab1
jamil ansari <jamilansari780@gmail.com>
2025-02-06 11:42:58 +0530

    dashboard component created

--- AngularRacipeApp/ClientApp/src/app/modules/layout/layout.module.ts
+++ AngularRacipeApp/ClientApp/src/app/modules/layout/layout.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
