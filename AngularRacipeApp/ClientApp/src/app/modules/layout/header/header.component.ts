f5dfb4fbb80fa2623bff4d8e257e9cb2e61ca062
jamil ansari <jamilansari780@gmail.com>
2025-02-10 19:41:54 +0530

    added toggled menu funtionality

--- AngularRacipeApp/ClientApp/src/app/modules/layout/header/header.component.ts
+++ AngularRacipeApp/ClientApp/src/app/modules/layout/header/header.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  title: string = "Recipes Management"
-  user: string = "admin"
-  displayStyle = "none"; 
+export class HeaderComponent implements OnInit {
+  title: string = 'Recipes Management';
+  user: string = 'admin';
+  displayStyle = 'none';
 
+  sideMenu: boolean = true;
+  @Output() sideNavToggled = new EventEmitter();
 
-  
-  openPopup() { 
-    this.displayStyle = "block"; 
-  } 
-  closePopup() { 
-    this.displayStyle = "none"; 
-  } 
-  
+  constructor() {}
+  ngOnInit(): void {}
+
+  sideNavToggle() {
+    this.sideMenu = !this.sideMenu;
+    this.sideNavToggled.emit(this.sideMenu);
+  }
+
+  openPopup() {
+    this.displayStyle = 'block';
+  }
+  closePopup() {
+    this.displayStyle = 'none';
+  }
 }
