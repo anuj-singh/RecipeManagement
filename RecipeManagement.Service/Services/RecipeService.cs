0ed0e7ea7b7d0daed9cbef52e0d3f8cd76a449be
svasundradevi <vasundradevi@gmail.com>
2025-02-14 17:27:04 +0530

    Added recipe upload code.

--- RecipeManagement.Service/Services/RecipeService.cs
+++ RecipeManagement.Service/Services/RecipeService.cs
@@ -1,36 +1,42 @@
 using RecipeManagement.Service.Interfaces;
 using RecipeManagement.Data.Interfaces;
 using RecipeManagement.Data.Models;
+using RecipeManagement.Service.Dtos;
+using System.Security;
 
 namespace RecipeManagement.Service.Services
 {
     public class RecipeService : IRecipeService
     {
         private readonly IRecipeRepository _recipeRepository;
-
-        public RecipeService(IRecipeRepository recipeRepository)
+        private readonly ILogService _logService;
+        public RecipeService(IRecipeRepository recipeRepository,ILogService logService)
         {
             _recipeRepository = recipeRepository;
+            _logService= logService;
         }
 
         public async Task<List<Recipe>> GetAllRecipesAsync()
         {
+            List<Recipe> lstRecipe= new  List<Recipe>();
             try
             {
-                return await _recipeRepository.GetAllRecipesAsync();
+                lstRecipe= await _recipeRepository.GetAllRecipesAsync();
             }
             catch (Exception ex)
             {
-                throw new ApplicationException("An error occurred while retrieving all recipes.", ex);
+                await  _logService.CreateLogAsync(ex.Message,"GetAllRecipesAsync");
             }
+            return lstRecipe;
         }
 
         // Get a recipe by its ID
         public async Task<Recipe> GetRecipeByIdAsync(int id)
         {
+            Recipe recipe= new  Recipe();
             try
             {
-                return await _recipeRepository.GetRecipeByIdAsync(id);
+                recipe= await _recipeRepository.GetRecipeByIdAsync(id);
             }
             catch (KeyNotFoundException ex)
             {
@@ -38,42 +44,53 @@ namespace RecipeManagement.Service.Services
             }
             catch (Exception ex)
             {
-                throw new ApplicationException($"An error occurred while retrieving recipe with ID {id}.", ex);
+                await  _logService.CreateLogAsync(ex.Message,"GetRecipeByIdAsync");
             }
+            return recipe;
         }
 
         // Get all recipes by a specific user ID
         public async Task<List<Recipe>> GetAllRecipebyUserIdAsync(int userId)
         {
+            List<Recipe> lstRecipe= new  List<Recipe>();
             try
             {
-                return await _recipeRepository.GetRecipesByUserIdAsync(userId);
+                lstRecipe= await _recipeRepository.GetRecipesByUserIdAsync(userId);
             }
             catch (Exception ex)
             {
-                throw new ApplicationException($"An error occurred while retrieving recipes for User ID {userId}.", ex);
+                 await  _logService.CreateLogAsync(ex.Message,"GetRecipeByIdAsync");
             }
+            return lstRecipe;
         }
 
         // Create a new recipe
-        public async Task<Recipe> CreateRecipeAsync(Recipe recipe)
+        public async Task<CommonResponseDto> CreateRecipeAsync(Recipe recipe)
         {
+            CommonResponseDto responseDto= new  CommonResponseDto();
             try
             {
-                return await _recipeRepository.AddRecipeAsync(recipe);
+                var recipeData= await _recipeRepository.AddRecipeAsync(recipe);
+                if(recipeData!= null)
+                {
+                    responseDto.Message="Recipe added successfully";
+                    responseDto.Status=true;
+                }
             }
             catch (Exception ex)
             {
-                throw new ApplicationException("An error occurred while creating the recipe.", ex);
+                await  _logService.CreateLogAsync(ex.Message,"CreateRecipeAsync");
             }
+            return responseDto;
         }
 
         // Update an existing recipe
         public async Task<Recipe> UpdateRecipeAsync(int id, Recipe recipe)
         {
+            Recipe recipeData=new Recipe();
             try
             {
-                return await _recipeRepository.UpdateRecipeAsync(id, recipe);
+                recipeData= await _recipeRepository.UpdateRecipeAsync(id, recipe);
             }
             catch (KeyNotFoundException ex)
             {
@@ -81,21 +98,33 @@ namespace RecipeManagement.Service.Services
             }
             catch (Exception ex)
             {
-                throw new ApplicationException($"An error occurred while updating recipe with ID {id}.", ex);
+                await  _logService.CreateLogAsync(ex.Message,"UpdateRecipeAsync");
             }
+            return recipeData;
         }
 
         // Delete a recipe by its ID
-        public async Task<bool> DeleteRecipeAsync(int id)
+        public async Task<CommonResponseDto> DeleteRecipeAsync(int id)
         {
+            CommonResponseDto responseDto= new CommonResponseDto();
             try
             {
-                return await _recipeRepository.DeleteRecipeAsync(id);
+                var result=  await _recipeRepository.DeleteRecipeAsync(id);
+                if(result)
+                {
+                    responseDto.Message="Recipe deleted successfully";
+                    responseDto.Status=true;
+                }else
+                {
+                    responseDto.Message="Recipe not deleted";
+                    responseDto.Status= false;
+                }
             }
             catch (Exception ex)
             {
-                throw new ApplicationException($"An error occurred while deleting recipe with ID {id}.", ex);
+                await  _logService.CreateLogAsync(ex.Message,"DeleteRecipeAsync");
             }
+            return responseDto;
         }
     }
 }
