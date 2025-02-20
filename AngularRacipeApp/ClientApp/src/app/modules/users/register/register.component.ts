dbdc3c2818ee2f7f508f77f598ff1ca3cfdcf40a
dharitribiswal <dharitri.b2912@gmail.com>
2025-02-20 14:16:32 +0530

    register api integration code modified

--- AngularRacipeApp/ClientApp/src/app/modules/users/register/register.component.ts
+++ AngularRacipeApp/ClientApp/src/app/modules/users/register/register.component.ts
@@ -10,67 +10,68 @@ import { DataService } from 'src/app/shared/service/data.service';
 })
 export class RegisterComponent implements OnInit {
 
-  
-constructor(
-  private router: Router, 
-  private dataService: DataService 
-){}
 
-ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+
+  registerForm = new FormGroup({
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern("[a-zA-Z].*")
+    ]
+    ),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15)
+    ]
+    ),
+  })
+
+  registerSubmitted() {
+
+    let registerUserObj = {
+      loginDto: {
+        userName: this.registerForm.controls['userName'].value,
+        email: this.registerForm.controls['email'].value,
+        password: this.registerForm.controls['password'].value
+      },
+      securityQuestion: "nickname",
+      securityAnswer: "test"
+    }
+    //console.log(JSON.stringify(registerUserObj, null , 2));
+    this.dataService.httpPostRequest('Auth/Register', registerUserObj)
+      .subscribe((res) => {
+        console.log(res);
+        this.router.navigate(['/users/login'])
+      });
+  }
+
+  get UserName(): FormControl {
+    return this.registerForm.get("userName") as FormControl;
+  }
+  get Email(): FormControl {
+    return this.registerForm.get("email") as FormControl;
+  }
+  get Password(): FormControl {
+    return this.registerForm.get("password") as FormControl;
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/users/']);
+  }
+
+
+
 }
 
 
-registerForm=new FormGroup({
-  userName:new FormControl('',[
-                                Validators.required,
-                                Validators.minLength(2),
-                                Validators.pattern("[a-zA-Z].*")
-                              ]
-                          ),
-  email:new FormControl('',[Validators.required]),
-  password:new FormControl('',[
-                                Validators.required,
-                                Validators.minLength(6),
-                                Validators.maxLength(15)
-                              ]
-                          ),
-})
-
-registerSubmitted(){
-  debugger;
-//  console.log(this.registerForm.value);
-//  this.dataService.httpPostRequest(this.router.navigate(['/main-dashboard']),
-//                                   this.registerForm.value).subscribe((response)=>{
-//  console.log(response);
-
- this.dataService
- .httpPostRequest('Auth/Register', this.registerForm.value)
- .subscribe((res) => {
-   console.log(res);
-  //  sessionStorage.setItem('tokenKey', JSON.stringify(res));
-   this.router.navigate(['/users/login'])
- });
-
- //this.dataService.httpPostRequest('',this.registerForm.value).subscribe((response)=>{
- // console.log(response);
-//})
-//this.router.navigate(['/main-dashboard']);
-}
-
-get UserName():FormControl{
-  return this.registerForm.get("userName")as FormControl;
-}
-get Email():FormControl{
-  return this.registerForm.get("email")as FormControl;
-}
-get Password():FormControl{
-  return this.registerForm.get("password")as FormControl;
-}
-
-navigateToLogin() {
-  this.router.navigate(['/users/']);
-}
-
-
-
-}
