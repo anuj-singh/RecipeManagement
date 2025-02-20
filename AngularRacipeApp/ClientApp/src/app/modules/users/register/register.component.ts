7a113712c54bac5b037091e898ef5802ab6e22f9
dharitribiswal <dharitri.b2912@gmail.com>
2025-02-20 11:58:07 +0530

    register api integration

--- AngularRacipeApp/ClientApp/src/app/modules/users/register/register.component.ts
+++ AngularRacipeApp/ClientApp/src/app/modules/users/register/register.component.ts
@@ -9,32 +9,48 @@ import { DataService } from 'src/app/shared/service/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
   
-constructor(private router: Router, private dataService: DataService ){
-  
-}
+constructor(
+  private router: Router, 
+  private dataService: DataService 
+){}
 
 ngOnInit(): void {
 }
 
+
 registerForm=new FormGroup({
-  username:new FormControl('',[
-      Validators.required,
-      Validators.minLength(2),
-      Validators.pattern("[a-zA-Z].*")]),
+  userName:new FormControl('',[
+                                Validators.required,
+                                Validators.minLength(2),
+                                Validators.pattern("[a-zA-Z].*")
+                              ]
+                          ),
   email:new FormControl('',[Validators.required]),
   password:new FormControl('',[
-    Validators.required,
-    Validators.minLength(6),
-    Validators.maxLength(15)
-  ]),
+                                Validators.required,
+                                Validators.minLength(6),
+                                Validators.maxLength(15)
+                              ]
+                          ),
 })
 
 registerSubmitted(){
- console.log(this.registerForm.value);
- this.dataService.httpPostRequest(this.router.navigate(['/main-dashboard']),this.registerForm.value).subscribe((response)=>{
- console.log(response);
-})
+  debugger;
+//  console.log(this.registerForm.value);
+//  this.dataService.httpPostRequest(this.router.navigate(['/main-dashboard']),
+//                                   this.registerForm.value).subscribe((response)=>{
+//  console.log(response);
+
+ this.dataService
+ .httpPostRequest('Auth/Register', this.registerForm.value)
+ .subscribe((res) => {
+   console.log(res);
+  //  sessionStorage.setItem('tokenKey', JSON.stringify(res));
+   this.router.navigate(['/users/login'])
+ });
+
  //this.dataService.httpPostRequest('',this.registerForm.value).subscribe((response)=>{
  // console.log(response);
 //})
@@ -42,7 +58,7 @@ registerSubmitted(){
 }
 
 get UserName():FormControl{
-  return this.registerForm.get("username")as FormControl;
+  return this.registerForm.get("userName")as FormControl;
 }
 get Email():FormControl{
   return this.registerForm.get("email")as FormControl;
@@ -54,5 +70,7 @@ get Password():FormControl{
 navigateToLogin() {
   this.router.navigate(['/users/']);
 }
+
+
 
 }
